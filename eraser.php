1b953505a431874e4310605cd5f4f432ae5b55eb
lifankohome <lifankohome@163.com>
2017-11-23 21:13:54 +0800

    添加 热搜排名，提示交互 修改 歌词样式

--- eraser.php
+++ eraser.php
@@ -15,7 +15,8 @@ $Eraser = new Meting($source);
 
 $Eraser->format(true); // 启用格式化功能
 
-function jsonKeyClear($json) {  //去掉jsonKey的引号，适应js数据格式
+function jsonKeyClear($json)
+{  //去掉jsonKey的引号，适应js数据格式
     $json = preg_replace('/"(\w+)"(\s*:\s*)/is', '$1$2', $json);   //去掉key的双引号
     return $json;
 }
@@ -40,12 +41,34 @@ switch (getParam('type')) {
             $name = $value['name'];
             $artist = $value['artist'][0];
 
-            $song = ['title'=>$name,'author'=>$artist,'url'=>$url,'pic'=>$pic,'lrc'=>$lrc];
+            $song = ['title' => $name, 'author' => $artist, 'url' => $url, 'pic' => $pic, 'lrc' => $lrc];
 
-            array_push($songList,$song);
+            array_push($songList, $song);
         }
 
         echo jsonKeyClear(json_encode($songList));
+
+        recordHotSearch($s);    //搜索词记录
+
+        break;
+    case 'hotSearch':  //获取热搜词
+        if(empty($max = getParam('max'))){ //显示的关键词数量
+            $max = 10;
+        }
+
+        $jsonHotSearch = hotSearch();
+        if (!empty($jsonHotSearch)) {
+            $arrHotSearch = json_decode($jsonHotSearch, true);  //解析为数组格式
+            arsort($arrHotSearch);  //按从多到少排序
+            $arrHotSearch = array_keys($arrHotSearch);  //将关键词（键）保存为新数组
+
+            $hotWordNum = count($arrHotSearch);
+            for ($i = 0; $i < ($hotWordNum > $max ? $max : $hotWordNum); $i++) {    //最多显示$max个热搜词
+                echo "<li>{$arrHotSearch[$i]}</li>";
+            }
+        } else {  //文件为空
+            echo "<li>无</li>";
+        }
 
         break;
     default:
@@ -55,4 +78,51 @@ switch (getParam('type')) {
 function getParam($key, $value = '')
 {
     return trim($key && is_string($key) ? (isset($_POST[$key]) ? $_POST[$key] : (isset($_GET[$key]) ? $_GET[$key] : $value)) : $value);
+}
+
+function recordHotSearch($hotWord)
+{
+    $jsonHotSearch = hotSearch();
+
+    if (!empty($jsonHotSearch)) {
+        $arrHotSearch = json_decode($jsonHotSearch, true);  //解析为数组格式
+        if (array_key_exists($hotWord, $arrHotSearch)) { //有记录则加一
+            $arrHotSearch[$hotWord] += 1;
+        } else {  //无记录则在数组中创建
+            $arrHotSearch[$hotWord] = 1;
+        }
+
+        $jsonHotSearch = json_encode($arrHotSearch);
+
+        hotSearch($jsonHotSearch);
+    } else {  //文件为空
+        $arrHotSearch = [$hotWord => 1];
+        $jsonHotSearch = json_encode($arrHotSearch);
+        hotSearch($jsonHotSearch);
+    }
+}
+
+function hotSearch($new = '')
+{
+    $filePath = "hotSearch.txt";
+
+    if (file_exists($filePath)) {
+        $fp = fopen($filePath, "r");
+        $str = fread($fp, filesize($filePath));     //指定读取大小，这里把整个文件内容读取出来
+
+        if (empty($new)) {  //$new为空时是读取状态，不为空时为写入状态
+            fclose($fp);
+
+            return $str;
+        } else {
+            $fp = fopen($filePath, "w");
+            flock($fp, LOCK_EX);
+            fwrite($fp, $new);
+            flock($fp, LOCK_UN);
+            fclose($fp);
+
+            return true;
+        }
+    }
+    return false;
 }
